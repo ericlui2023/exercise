88f47b01595f0838be01ed857d873f20b45042cb
elui <21458650@life.hkbu.edu.hk>
2023-02-04 01:38:52 +0800

    test v1

--- a.py
+++ a.py
@@ -3,7 +3,3 @@ print ("hello")
 b = 50
 
 c = 100
-
-####  edit this file for branch test
-
-d = b+c
--- b.py
+++ b.py
@@ -3,7 +3,5 @@ for i in a:
     print(i)
     
 
-####  edit this file for branch test
-
-
-d = a**2
+    
+    
